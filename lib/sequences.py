3d8d841fe63f5d9b1e4d0131d3405a24be03c7da
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-20 16:42:07 +0300

    new

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # Initialize the list with the first two Fibonacci numbers
+    fibonacci_sequence = [0, 1]
+
+    # If the desired length is less than 2, slice the list accordingly
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    # Generate the Fibonacci sequence up to the desired length
+    while len(fibonacci_sequence) < length:
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+
+    # Print the resulting list up to the desired length
+    print(fibonacci_sequence[:length])
